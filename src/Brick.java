abaf50d8b3ae4f339c1602ceca10017e7b1d1d29
Austin Odell <awo6@duke.edu>
2020-01-14 11:58:40 -0500

    Added Brick class to extend Rectangle with Health

--- src/Brick.java
+++ src/Brick.java
@@ -5,11 +5,25 @@ import javafx.scene.shape.Shape;
 public class Brick extends Rectangle {
 
     public boolean isDead = false;
+    public int myHealth = 0;
 
+
+    public Brick(double x, double y, double width, double height){
+        super(x,y, width, height);
+    }
     public void kill(){
 
     }
     public boolean collide(Shape other) {
         return false;
+    }
+    public void setHealth(int health){
+        myHealth = health;
+    }
+    public int getHealth(){
+        return myHealth;
+    }
+    public void removeHealth(){
+        myHealth --;
     }
 }
