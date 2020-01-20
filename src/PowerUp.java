3723b457d33001f3286bf72d4a15d4cc3c886360
Austin Odell <awo6@duke.edu>
2020-01-20 00:11:53 -0500

    Limited it to 3 power-ups

--- src/PowerUp.java
+++ src/PowerUp.java
@@ -2,7 +2,7 @@ import javafx.scene.shape.Circle;
 
 public class PowerUp extends Circle {
     public static final int RADIUS = 5;
-    public static final int NUM_POWERS = 5;
+    public static final int NUM_POWERS = 3;
     public static final int VELOCITY = 200;
     private int powerType = 0;
 
